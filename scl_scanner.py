841f95e60444f13df678ad394d7f825b5366b01a
Ethan Butler <me@ethanbtlr.com>
2022-02-20 16:41:09 -0500

    Combined two files into one scanner

--- scl_scanner.py
+++ scl_scanner.py
@@ -2,8 +2,6 @@ import sys
 import json
 
 
-
-
 # Symbol Table
 # Keywords
 KEYWORDS = [
@@ -33,12 +31,15 @@ KEYWORDS = [
 
 # Identifiers
 
+
 # Operators
 ASSIGNMENT_OP = ["="]
 ARITHMETIC_OP = ["+", "-", "*", "/"]
 RELATIONAL_OP = ["==", "!=", "<", ">", "<=", ">="]
 
 # Constants
+
+
 # Special Characters
 DELIMITER_CHARS = [" ", ",", "\t", '"']
 
@@ -47,11 +48,41 @@ DELIMITER_CHARS = [" ", ",", "\t", '"']
 def scan_file(input_file):
 
     # Retrieve the info from the file
-    with open(inputFile) as f:
-        print(f.read())
+    #reads in file
+
+    #opens file in read mode
+    f = open(input_file,"r")
+    contents = f.readlines()
+
+
+    for line in contents:
+        split = line.split()
+
+        for word in split:
+            stripped = word.strip()
+            print(word)
+
+    f.close()
+    return 0
+    
+    
 
 def output_syntax():
     print("Results: ")
 
 # Pass the user's input file into the scan file function
 scan_file(sys.argv[1])
+
+
+
+# List of tested files that were given and used
+''' 
+arduino_ex1.scl
+arrayex1.scl
+arrayex1b.scl
+arrayex4b.scl
+bitops1.scl
+datablistp.scl
+linkedg.scl
+welcome.scl
+'''
