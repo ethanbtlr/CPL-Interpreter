f4c705547b5e1aeef4e8ba537c900ee4152347a9
Ethan Butler <me@ethanbtlr.com>
2022-02-24 23:45:54 -0500

    Separated special characters into seperate tokens

--- scl_scanner.py
+++ scl_scanner.py
@@ -34,7 +34,9 @@ ARITHMETIC_OP = ["+", "-", "*", "/"]
 RELATIONAL_OP = ["==", "!=", "<", ">", "<=", ">="]
 
 # Special Characters
-DELIMITER_CHARS = [" ", ",", "\t", '"']
+DELIMITER_CHARS = [" ", ",", "\t", "\n", ":", ";", "(", ")"]
+
+ALL_CHARS__OPS = ASSIGNMENT_OP + ARITHMETIC_OP + RELATIONAL_OP + DELIMITER_CHARS
 
 
 # Scan the contents of the input file
@@ -87,15 +89,63 @@ def scan_file(input_file):
                 # Gets each word in each line and adds it to the token array
                 for word in line.split(" "):
 
-                    # If the word has newline token in it, split it up and add them both to the array
-                    position = word.find("\n")
-                    if position != -1:
-                        word = word[0:position]
-                        tokens.append(word)
-                        tokens.append("\n")
-                    else:
-                        tokens.append(word)
+                    # If the word has special character or operator in it, split the tokens up and add them both to the array
+                    hasSpecialChar = False
+                    for i in ALL_CHARS__OPS:
 
+                        # If a special character is found
+                        position = word.find(i)
+                        if position != -1:
+                            lengthOfWord = len(line)
+                            lengthOfChar = len(i)
+
+                            # Say a special character has been found
+                            hasSpecialChar = True
+
+                            # If special character is at the beginning
+                            if position == 0:
+                                # Append the special character
+                                tokens.append(i)
+
+                                # Append everything after the special character
+                                word = word[lengthOfChar:lengthOfWord]
+                                tokens.append(word)
+
+                            # If special character is at the end
+                            elif position == (lengthOfWord - 1):
+                                # Append everything before the special character
+                                word = word[0:position]
+                                tokens.append(word)
+
+                                # Append the special character
+                                tokens.append(i)
+
+                            # If a special character is in the middle
+                            else:
+                                # Append everything before the special character
+                                wordOne = word[0:position]
+                                tokens.append(wordOne)
+
+                                # Append the special character
+                                tokens.append(i)
+
+                                # Append everything after the special character
+                                wordTwo = word[(position + lengthOfChar) : lengthOfWord]
+
+                                # See if another special character is in the rest of the list
+                                hasAnotherSpecialChar = False
+                                for j in ALL_CHARS__OPS:
+                                    position = line.find(j)
+                                    if j != -1:
+                                        word = wordTwo
+                                        hasAnotherSpecialChar = True
+                                # If there isn't another special character just add the rest of the word to the token list
+                                if hasAnotherSpecialChar == False:
+                                    tokens.append(wordTwo)
+
+                    # If a special character isn't found
+                    if hasSpecialChar == False:
+                        tokens.append(word)
 
     JSON_export(tokens)
 
@@ -110,7 +160,7 @@ def JSON_export(tokens_list):
             break
 
     tokens = {"Tokens": tokens_list}
-    
+
     # Prints the tokens
     dump = json.dumps(tokens)
     print(dump)
@@ -126,6 +176,7 @@ def JSON_export(tokens_list):
 # Pass the user's input file into the scan file function
 try:
     scan_file(sys.argv[1])
+
 except IndexError as err:
     print("A file wasn't passed as an argument. Error: {0}".format(err))
 
@@ -133,11 +184,11 @@ except IndexError as err:
 # List of tested files that were given and used
 """ 
 arduino_ex1.scl
-# arrayex1.scl
-# arrayex1b.scl
-# arrayex4b.scl
-# bitops1.scl
-# datablistp.scl
-# linkedg.scl
-# welcome.scl
+arrayex1.scl
+arrayex1b.scl
+arrayex4b.scl
+bitops1.scl
+datablistp.scl
+linkedg.scl
+welcome.scl
 """
